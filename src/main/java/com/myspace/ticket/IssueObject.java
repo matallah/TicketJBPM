2606663346284c386a50c69d64b4663ac5689600
wbadmin <>
2021-01-21 21:02:07 +0200

    {/src/main/java/com/myspace/ticket/IssueObject.java}

--- src/main/java/com/myspace/ticket/IssueObject.java
+++ src/main/java/com/myspace/ticket/IssueObject.java
@@ -7,29 +7,89 @@ package com.myspace.ticket;
 @javax.persistence.Entity
 public class IssueObject implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "ISSUEOBJECT_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(name = "ISSUEOBJECT_ID_GENERATOR", sequenceName = "ISSUEOBJECT_ID_SEQ")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "ISSUEOBJECT_ID_GENERATOR")
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(name = "ISSUEOBJECT_ID_GENERATOR", sequenceName = "ISSUEOBJECT_ID_SEQ")
+	private java.lang.Long id;
 
-    public IssueObject() {
-    }
-    
-    public IssueObject(java.lang.Long id) {
-        this.id = id;
-    }
+	@org.kie.api.definition.type.Label(value = "Title")
+	private java.lang.String title;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	@org.kie.api.definition.type.Label(value = "Description")
+	private java.lang.String description;
 
+	@org.kie.api.definition.type.Label(value = "Closed")
+	private boolean closed;
 
+	@org.kie.api.definition.type.Label(value = "Creation date")
+	private java.time.LocalDateTime creationDate;
 
+	@org.kie.api.definition.type.Label(value = "Bodified date")
+	private java.time.LocalDateTime modifiedDate;
+
+	public IssueObject() {
+	}
+
+	public java.lang.Long getId() {
+		return this.id;
+	}
+
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
+
+	public java.lang.String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(java.lang.String title) {
+		this.title = title;
+	}
+
+	public java.lang.String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(java.lang.String description) {
+		this.description = description;
+	}
+
+	public boolean isClosed() {
+		return this.closed;
+	}
+
+	public void setClosed(boolean closed) {
+		this.closed = closed;
+	}
+
+	public java.time.LocalDateTime getCreationDate() {
+		return this.creationDate;
+	}
+
+	public void setCreationDate(java.time.LocalDateTime creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public java.time.LocalDateTime getModifiedDate() {
+		return this.modifiedDate;
+	}
+
+	public void setModifiedDate(java.time.LocalDateTime modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public IssueObject(java.lang.Long id, java.lang.String title,
+			java.lang.String description, boolean closed,
+			java.time.LocalDateTime creationDate,
+			java.time.LocalDateTime modifiedDate) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.closed = closed;
+		this.creationDate = creationDate;
+		this.modifiedDate = modifiedDate;
+	}
 
 }
